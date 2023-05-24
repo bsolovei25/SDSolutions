f443122606101c0b115b3ad4ed68925d0b8570ea
bsolovei25 <bsolovei25@gmail.com>
2023-05-24 13:03:38 +0300

    hash code moved minor error fixed

--- core/scripts/xml/parseToXML.js
+++ core/scripts/xml/parseToXML.js
@@ -3,9 +3,9 @@ function parseToXML(listBox) {
     let xmlArr = [];
     xmlArr.push('&lt;' + 'root'+'&gt;'+'<br/>');
     for (let i =0;i < listArr.length;i++) {
-        xmlArr.push('&lt;' + 'name'+'&gt;'+listArr[i][0]+'&lt;'+'name'+'&#47;'+'&gt;'+'&lt;' + 'value'+'&gt;'+listArr[i][1]+'&lt;'+'value'+'&#47;'+'&gt;'+'<br/>');
+        xmlArr.push('&lt;' + 'name'+'&gt;'+listArr[i][0]+'&lt;'+'&#47;'+'name'+'&gt;'+'&lt;' + 'value'+'&gt;'+listArr[i][1]+'&lt;'+'&#47;'+'value'+'&gt;'+'<br/>');
     }
-    xmlArr.push('&lt;' + 'root'+'&#47;'+'&gt;');
+    xmlArr.push('&lt;'+'&#47;' + 'root'+'&gt;');
     document.getElementById('error-box').style.display='none';
     document.getElementById('values-box').innerHTML = xmlArr.join("");
 }
